e3e1d79a146bc5d6bb0fab016ae26b270be0016d
hankyeol-jung <whd6023@gmail.com>
2022-10-27 23:29:39 +0900

    버튼에 기능개발을 해보자 & 리액트 state변경하는 법

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+/* eslint-disable */
+// 워닝메세지 없에기!
+
 import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 
 function App() {
   let post = "강남 우동 맛집";
-  let [글제목1, b1] = useState("남자 코트 추천");
-  let [글제목2, b2] = useState("여자 코트 추천");
-  let [글제목3, b3] = useState("아이 코트 추천");
+  let [글제목, b] = useState(["남자 코트 추천", "강남 우동맛집", "파이썬독학"]);
+  let [따봉, 따봉변경] = useState(0);
 
   return (
     <div className="App">
@@ -14,15 +16,34 @@ function App() {
         <h4>ReactBlog</h4>
       </div>
       <div className="list">
-        <h4>{글제목1}</h4>
+        <h4>
+          {글제목[0]}
+          <span
+            onClick={() => {
+              let copy = [...글제목];
+              copy[0] = "여자코트추천";
+              b(copy);
+            }}
+          >
+            글제목변경
+          </span>
+          <span
+            onClick={() => {
+              따봉변경(따봉 + 1);
+            }}
+          >
+            👍
+          </span>
+          {따봉}
+        </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{글제목2}</h4>
+        <h4>{글제목[1]}</h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{글제목3}</h4>
+        <h4>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
     </div>
